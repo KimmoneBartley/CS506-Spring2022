9b0f918360d00bb274264703e08abcca636200f7
Jason Yeung <yeung.jsn@gmail.com>
2022-02-01 14:46:34 -0500

    Adding power plant image and .py

--- 01-python/infrastructure/power.py
+++ 01-python/infrastructure/power.py
@@ -1,3 +1,21 @@
+
+from PIL import Image, ImageDraw
+import os
+import tkinter
+import io
+
 def draw_power_plant():
-    print("power plant not found")
+    # power_image = Image.new('RGB', (500, 300), (128, 128, 128))
+    # draw = ImageDraw.Draw(power_image)
+    # draw.rectangle((200, 100, 300, 200), fill=(0, 192, 192), outline=(255, 255, 255))
+    # draw.rectangle((300, 200, 500, 400), fill=(60, 90, 50), outline=(255, 255, 255))
+    # power_image.save('power_example.png', quality=95)
+    # img = Image.open("power_example.png")
+    # img.show()
+
+    img = Image.open(os.getcwd() + '/power.jpg')
+    img.thumbnail((600, 400))
+    img.show()
     return
+
+draw_power_plant()

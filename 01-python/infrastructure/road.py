fb04d767e6903a62b10f189faed85551b19c5d04
sijieye <sijieye@bu.edu>
2022-02-01 14:44:13 -0500

    add road image

--- 01-python/infrastructure/road.py
+++ 01-python/infrastructure/road.py
@@ -1,3 +1,18 @@
 def draw_road():
     print("road not found")
     return
+
+
+from PIL import Image
+
+# Read image
+img = Image.open('road.jpg')
+
+# Output Images
+img.show()
+
+# prints format of image
+print(img.format)
+
+# prints mode of image
+print(img.mode)
